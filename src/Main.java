c377f22294a5744b32da8bdaed2629cbb0efb0b4
Evgeny Pimenov <evgenypimenov@Evgeny-MacBook-Air.local>
2023-06-11 13:44:44 +0300

    Test commit v.1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {
+                15,
+                27,
+                3,
+                344,
+                17
+        };
+        int summ = 0;
+        for (int i : arr) {
+            summ += i;
+        }
+        System.out.println(summ / arr.length);
+    }
+}
